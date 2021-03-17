5af385abdfee1e595025d2136633074147e1c332
Your Username <you@example.com>
2021-03-17 10:31:26 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,10 @@
+class Dog
+end
+
+def bark
+    "puts Woof!"
+end 
+
+def sit
+    "puts Dog is sitting"
+end
---
+++ lib/person.rb
@@ -0,0 +1,10 @@
+class person
+end 
+
+def talk
+    puts "Hello World"
+end 
+
+def walk
+    puts "The Person is walking"
+end 
